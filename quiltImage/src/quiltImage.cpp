f1b0536911d9a93b0249eeaf1ef14b9c48bf7db1
ychding11 <ych_ding@163.com>
2018-05-06 01:03:49 +0800

    first time commit for quiltImage.

--- quiltImage/src/quiltImage.cpp
+++ quiltImage/src/quiltImage.cpp
@@ -64,42 +64,13 @@ int main(int argc, char **argv)
     printf("- Image size w=%d, h=%d.\n", inputImage.cols, inputImage.rows);
 	int h = inputImage.rows;
 	int w = inputImage.cols;
+	cv::Mat dstM(cv::Size(w * 3, h * 2), inputImage.type(), cv::Scalar::all(0));
+	cv::Mat roiM(dstM, cv::Rect(0, 0, w, h));
+	inputImage.copyTo(roiM);
+	roiM = dstM(cv::Rect(w, 0, w * 2, h * 2));
 
-	int kw = 31;
-	int kh = 11;
+	naiveSynthesis(inputImage, 7).copyTo(roiM);
 
-    cv::Mat patch1 (inputImage, cv::Rect(0, 0, kw, kh));
-	cv::Mat patch2 (inputImage, cv::Rect(w - kw, h - kh, kw, kh));
-	
-    printf("- patch1 size w=%d, h=%d.\n", patch1.cols, patch1.rows);
-    printf("- patch2 size w=%d, h=%d.\n", patch2.cols, patch2.rows);
-
-	cv::Mat dstM(cv::Size(inputImage.cols * 2, inputImage.rows), inputImage.type(), cv::Scalar::all(0));
-    cv::Mat roiM (dstM, cv::Rect(0, 0, w, h));
-
-	//std::cout << "\n inputImage = \n \t" <<  inputImage;
-    //std::cout << "\n patch1 =\n\t " << patch1 ;
-    //std::cout << "\n patch2 =\n\t " << patch2 ;
-
-	cv::namedWindow("Input Image");
-    cv::imshow("Input Image", inputImage);
-	//cv::imshow("Patch1", patch1);
-	//cv::imshow("Patch2", patch2);
-    //cv::imshow("Synthesis Image", naiveSynthesis(inputImage, 7));
-	cv::waitKey(); // wait for key stroke forever.
-
-	for (int i = 0; i < kh; ++i)
-	{
-		for (int j = 0; j < kw; ++j)
-		{
-			//patch1.at<cv::Scalar>(i, j) = cv::Scalar(255, 0, 0);
-			//patch2.at<cv::Scalar>(i, j) = cv::Scalar(0, 0, 255);
-			patch1.at<cv::Vec3b>(i, j) = cv::Vec3b(255, 0, 0);
-			patch2.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0, 255);
-		}
-	}
-    cv::imshow("Input Image", inputImage);
-	cv::waitKey(); // wait for key stroke forever.
-
+    cv::imshow("Synthesis Image", dstM); cv::waitKey(); // wait for key stroke forever.
 	return 0;
 }
